db2bc4e43de64cc630940f0eb00e023f14abe40b
maki <marekmako@gmail.com>
2016-08-09 15:11:13 +0200

    socialviewdelegat

--- DiaSport/Social.swift
+++ DiaSport/Social.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class SocialViewController: UIViewController {
+class SocialViewController: UIViewController, SocialViewDelegate {
     
     // MARK: IBAction
     
@@ -18,10 +18,34 @@ class SocialViewController: UIViewController {
     }
     
     // MARK: IBOutlet
+    
+    @IBOutlet weak var socialView: SocialView! {
+        didSet {
+            socialView.delegate = self
+        }
+    }
+    
+    // MARK: SocialViewDelegate
+    
+    func onTwitterShare() {
+        print("onTwitterShare")
+    }
 }
 
 
+
+@objc protocol SocialViewDelegate {
+    optional func onTwitterShare()
+    optional func onFacebookShare()
+    optional func onGoogleShare()
+    optional func onPinterestShare()
+}
+
+
+
 class SocialView: UIView {
+    
+    var delegate: SocialViewDelegate?
     
     let buttonWidth: CGFloat = 80
     let buttonHeight: CGFloat = 80
@@ -36,15 +60,19 @@ class SocialView: UIView {
         super.init(coder: aDecoder)
         
         twitterButton.setBackgroundImage(UIImage(named: "twitter"), forState: .Normal)
+        twitterButton.addTarget(self, action: #selector(onTwitterShare), forControlEvents: .TouchUpInside)
         addSubview(twitterButton)
         
         facebookButton.setBackgroundImage(UIImage(named: "facebook"), forState: .Normal)
+        facebookButton.addTarget(self, action: #selector(onFacebookShare), forControlEvents: .TouchUpInside)
         addSubview(facebookButton)
         
         googleButton.setBackgroundImage(UIImage(named: "google"), forState: .Normal)
+        googleButton.addTarget(self, action: #selector(onGoogleShare), forControlEvents: .TouchUpInside)
         addSubview(googleButton)
         
         pinterestButton.setBackgroundImage(UIImage(named: "pinterest"), forState: .Normal)
+        pinterestButton.addTarget(self, action: #selector(onPinterestShare), forControlEvents: .TouchUpInside)
         addSubview(pinterestButton)
     }
     
@@ -53,5 +81,23 @@ class SocialView: UIView {
         facebookButton.frame = CGRectMake(bounds.width / 4, bounds.height / 2.5, buttonWidth, buttonHeight)
         googleButton.frame = CGRectMake(bounds.width / 2, bounds.height / 1.8, buttonWidth, buttonHeight)
         pinterestButton.frame = CGRectMake(bounds.width / 1.5, bounds.height / 3.5, buttonWidth, buttonHeight)
+    }
+    
+    // MARK: Support SocialViewDelegate
+    
+    func onTwitterShare() {
+        delegate?.onTwitterShare?()
+    }
+    
+    func onFacebookShare() {
+        delegate?.onFacebookShare?()
+    }
+    
+    func onGoogleShare() {
+        delegate?.onGoogleShare?()
+    }
+    
+    func onPinterestShare() {
+        delegate?.onPinterestShare?()
     }
 }
